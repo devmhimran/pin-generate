2ff66f1665b108d496cf75ea0f7a65ee11d227af
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-02-15 13:03:44 +0600

    Added Verify Action

--- main.js
+++ main.js
@@ -13,8 +13,7 @@ function generateNum(){
 document.getElementById('generate-pin-btn').addEventListener('click', function(){
     const generateRandomNum = generateNum();
     document.getElementById('generate-pin-input').value = generateNum();
-    
-    console.log(generateRandomNum);
+    document.getElementById('submitBtn').removeAttribute('disabled');
 });
 
 document.getElementById('keypad').addEventListener('click', function(e){
@@ -31,27 +30,43 @@ document.getElementById('keypad').addEventListener('click', function(e){
     }
     
 
-    console.log(keypadNum);
+    // console.log(keypadNum);
 });
 
     const failedMsg = document.getElementById('failedMsg');
     const successMsg = document.getElementById('successMsg');
+    var clickCount = 0
     failedMsg.style.display = 'none';
     successMsg.style.display = 'none';
-
-document.getElementById('submitBtn').addEventListener('click', function(){
-    const matchPinInput = document.getElementById('match-pin-input');
-    const randomNum = document.getElementById('generate-pin-input').value;
-    const failedMsg = document.getElementById('failedMsg');
-    const successMsg = document.getElementById('successMsg');
     
-    const typedNum = matchPinInput.value;
-    if( typedNum == randomNum){
-        successMsg.style.display = 'block';
-        failedMsg.style.display = 'none';
-    }else{
-        failedMsg.style.display = 'block';
-        successMsg.style.display = 'none';
+document.getElementById('submitBtn').addEventListener('click', function(){
+    clickCount = clickCount+1;
+
+    if(clickCount == 3){
+        const submitAction = document.getElementById('submit-action');
+        document.getElementById('submitBtn').setAttribute('disabled', true);
+        submitAction.innerText = 3 - clickCount;
+        console.log(submitAction.innerText);
+    }else if(clickCount > 0){
+        const matchPinInput = document.getElementById('match-pin-input');
+        const randomNum = document.getElementById('generate-pin-input').value;
+        const failedMsg = document.getElementById('failedMsg');
+        const successMsg = document.getElementById('successMsg');
+        
+        const typedNum = matchPinInput.value;
+        if( typedNum == randomNum){
+            successMsg.style.display = 'block';
+            failedMsg.style.display = 'none';
+        }else{
+            failedMsg.style.display = 'block';
+            successMsg.style.display = 'none';
+        }
+        const submitAction = document.getElementById('submit-action');
+        submitAction.innerText = 3 - clickCount;
+        
+        matchPinInput.value = '';
     }
-    matchPinInput.value = '';
+    
+    
+    
 });
